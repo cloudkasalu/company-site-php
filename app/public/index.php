3a49253f475b0de3b6a0856b0339c99306c2623b
Cloud <65218174+cloudkasalu@users.noreply.github.com>
2023-05-02 16:13:45 +0200

    server config test

--- app/public/index.php
+++ app/public/index.php
@@ -1,13 +1,15 @@
 <?php 
 
+echo "hello world";
+
 
 // include_once '../classes/EntryPoint.php';
-include __DIR__ . '/../includes/autoload.php';
-include_once __DIR__ . '/../includes/global.php';
+// include __DIR__ . '/../includes/autoload.php';
+// include_once __DIR__ . '/../includes/global.php';
 
 
-$url = strtok(ltrim($_SERVER['REQUEST_URI'], '/'), '?');
+// $url = strtok(ltrim($_SERVER['REQUEST_URI'], '/'), '?');
 
-$entryPoint = new \classes\EntryPoint();
-$entryPoint->run($url);
+// $entryPoint = new \classes\EntryPoint();
+// $entryPoint->run($url);
 
